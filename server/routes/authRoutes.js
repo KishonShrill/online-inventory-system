e3b9e9d261d691154b6afde04844f1028f1e4a65
KishonShrill <crystalbluew@gmail.com>
2025-06-24 15:29:07 +0800

    CI: Deploying server to vercel, authentication

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,82 @@
+import e from "express";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/models.js';
+
+const router = e.Router();
+
+router.post('/api/register', async (req, res) => {
+    if (!req.body || !req.body.email || !req.body.password || !req.body.secret_code) {
+        return res.status(400).json({ message: "Email and password required" });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        const result = await user.save();
+        res.status(201).json({
+            message: "User Created Successfully",
+            result,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            error: "Error creating user",
+            message: error.message,
+        });
+    }
+});
+
+
+router.post('/api/login', async (req, res) => {
+    try {
+        // check if email exists
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "Email not found",
+            });
+        }
+
+        // compare the password entered and the hashed password found
+        const passwordCheck = await bcrypt.compare(req.body.password, user.password);
+
+        if (!passwordCheck) {
+            return res.status(400).json({
+                message: "Passwords does not match",
+            });
+        }
+
+        //   create JWT token
+        const token = jwt.sign(
+            {
+                userName: user.name,
+                userEmail: user.email,
+            },
+            process.env.JWT_SECRET || "RANDOM-TOKEN", // Use env variable for secret
+            { expiresIn: "24h" }
+        );
+
+        //   return success res
+        res.status(200).json({
+            message: "Login Successful",
+            email: user.email,
+            token,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error during login",
+            error: error.message,
+        });
+    }
+})
+
+export default router;
